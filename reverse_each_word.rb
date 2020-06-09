20a73188892e01de7d600d859e46ce8417a6158b
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 03:48:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence1)
   phrase = sentence1.split(" ")             #converting the string sentence1 into a new array called phrase.
   backward_array = []                       #Creating an empty array
-  phrase.each do |sentence|                #using iterator .each on the phrase array
-    backward_array << sentence.reverse 
+  phrase.each do |greeting|                #using iterator .each on the phrase array
+    backward_array << greeting.reverse 
   end
   backward_array.join(" ")
 end
