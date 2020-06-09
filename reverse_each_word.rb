fbd6e9c4bbb4568a1c09a6db0d28a8b1e0217c46
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 03:59:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence1)            #defining the method with the string argument
   phrase = sentence1.split(" ")             #converting the string sentence1 into a new array called phrase.
   backward_array = []                       #Creating an empty array
-  phrase.each do |greeting|                #using iterator .each on the phrase array
-    backward_array << greeting.reverse 
-  end
-  backward_array.join(" ")
-end
+  phrase.each do |greeting|                 #invoking iterator .each on the phrase array with greeting local variable
+    backward_array << greeting.reverse      #shoveling each reverse variable from the phrase into the new empty array
+  end                                       #closing the block
+  backward_array.join(" ")                  #converting the array back into a string
+end                                         #closing the definition
 
 def reverse_each_word(sentence2)
   phrase = sentence2.split(" ")
