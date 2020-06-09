2da994c0b30ad8062bf82140e2a59ebea2502e63
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-06-09 04:20:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)  
+def reverse_each_word(sentence1)  
   
-  phrase1 = sentence.split(" ")             
+  phrase1 = sentence1.split(" ")             
   backward_array1 = []                       
   phrase1.each {|greeting1| backward_array1 << greeting1.reverse}
   backward_array1.join(" ")
 
-  phrase2 = sentence.split(" ")             
+  phrase2 = sentence1.split(" ")             
   backward_array2 = []                       
   phrase2.collect {|greeting2| backward_array2 << greeting2.reverse}
   backward_array2.join(" ")
